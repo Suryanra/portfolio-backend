d9cda850db6ba08ec6a7e762f36a132fb972a159
suryanra <suryapratapnra@gmail.com>
2024-10-18 16:21:04 +0530

    adding achievement

--- data/achievement.js
+++ data/achievement.js
@@ -1,10 +1,17 @@
       const achievementData = [
         {
+          title: "Achieved 1st position in Hackathon conducted by Zeal Global Inc",
+          date: "28 september 2024",
+          details:
+            "Me and My team participated in hackthon conducted by Zeal global inc and have Achieved 1 rank",
+          link: "https://drive.google.com/file/d/1VpVNfLFU-O8cMPEcxmWoeLeiiCE04csh/view?usp=drive_link"
+        },
+        {
           title: "UBS Hackathon - 3rd Prize out of 400 Teams",
           date: "5 March 2024",
           details:
             "My team and I participated in the hackathon conducted by Union Bank of Switzerland, competing against approximately 400 teams. We won 3rd prize. My contributions included developing a real-time chat forum, integrating Razorpay for payment processing, creating interactive games to enhance cognitive and problem-solving skills, and developing a blog in both video and text formats.",
-          link: "https://www.linkedin.com/posts/suryanra_ubshackathon-empoweringparents-innovation-activity-7182086464275456004-3JCG?utm_source=share&utm_medium=member_desktop",
+          link: "https://drive.google.com/file/d/1qbYNsBcphrRwHnu6Izpsl7otj-nzrBQb/view?usp=drive_link",
         },
         {
           title: "2 ⭐⭐ on CodeChef",
@@ -13,7 +20,7 @@
           link: "https://www.codechef.com/users/suryapratapnra",
         },
         {
-          title: "Solved 200+ DSA Problems on Various Platforms",
+          title: "Solved 300+ DSA Problems on Various Platforms",
           date: "until 5 June 2024",
           details:
             "I have solved over 200 Data Structures and Algorithms problems on different platforms.",
